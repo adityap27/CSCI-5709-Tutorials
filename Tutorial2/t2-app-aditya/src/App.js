0f90b169e8f2ba39686caeee6f253dc5156ccd47
Aditya Maheshbhai Purohit <aditya.purohit@dal.ca>
2024-01-26 23:57:42 -0400

    add Tutorial2 react app basic code

---
+++ Tutorial2/t2-app-aditya/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          It Works!
+        </p>
+        <p>
+          CSCI 5709 - Advanced Topics in Web Development - Tutorial 2
+        </p>
+        <p>
+          Aditya Maheshbhai Purohit, B00952865
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
